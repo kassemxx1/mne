0d9eafce232abaa7be407a929037ae8be1384073
kassem <kassemxx1>
2019-11-27 00:11:24 +0200

    all done maybe

--- lib/clientsScreen.dart
+++ lib/clientsScreen.dart
@@ -1,10 +1,13 @@
 import 'package:autocomplete_textfield/autocomplete_textfield.dart';
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dropdown_formfield/dropdown_formfield.dart';
 import 'constants.dart';
 
 final _firestore = Firestore.instance;
+var name = '';
+var phonenumber = '';
 
 class ClientsScreen extends StatefulWidget {
   static const String id = 'Clients_Screen';
@@ -13,9 +16,12 @@ class ClientsScreen extends StatefulWidget {
 }
 
 class _ClientsScreenState extends State<ClientsScreen> {
+  var transaction = [];
   TextEditingController _textEditingController1 = TextEditingController();
   TextEditingController _textEditingController2 = TextEditingController();
-  TextEditingController _textEditingController3= TextEditingController();
+  TextEditingController _textEditingController3 = TextEditingController();
+  TextEditingController _textEditingController4 = TextEditingController();
+  TextEditingController _textEditingController5 = TextEditingController();
   String mytext = '';
   String currentText = "";
   var ListOfItems = [];
@@ -23,9 +29,9 @@ class _ClientsScreenState extends State<ClientsScreen> {
   var itemValue = '';
   var listcat = ['phones', 'recharge', 'accessories'];
   var catlist = [];
-  var qtt = 0.0;
+  var qtt = 1.0;
   var debt = 0.0;
-  var DebtAnalysis=0.0;
+  var DebtAnalysis = 0.0;
 
   void getcat() {
     catlist.clear();
@@ -54,24 +60,46 @@ class _ClientsScreenState extends State<ClientsScreen> {
     }
   }
 
+  void gettransactiondate(String name) async {
+    final messages = await _firestore
+        .collection('clients')
+        .where('name', isEqualTo: name)
+        .getDocuments();
+    transaction.clear();
+    for (var msg in messages.documents) {
+      final debt = msg.data['debt'];
+      final den = msg.data['den'];
+      final product = msg.data['product'];
+      final time = msg.data['timestamp'];
+      final ttime = msg.documentID;
+      setState(() {
+        transaction.add({
+          'debt': debt,
+          'den': den,
+          'product': product,
+          'time': time,
+          'id': ttime,
+        });
+      });
+    }
+  }
+
   GlobalKey<AutoCompleteTextFieldState<String>> key = new GlobalKey();
 
   _ClientsScreenState() {
     textField = SimpleAutoCompleteTextField(
-      key: key,
-      controller: TextEditingController(text: mytext),
-      suggestions: suggestions,
-      textChanged: (text) => currentText = text,
-      submitOnSuggestionTap: true,
-      textSubmitted: (text) {
-        setState(() {
-          mytext = text;
-
+        key: key,
+        controller: TextEditingController(text: mytext),
+        suggestions: suggestions,
+        textChanged: (text) => currentText = text,
+        submitOnSuggestionTap: true,
+        textSubmitted: (text) {
+          setState(() {
+            mytext = text;
+          });
+          getqtt(mytext);
+          gettransactiondate(mytext);
         });
-        getqtt(mytext);
-      }
-
-    );
   }
   Future<double> getqtt(String name) async {
     var qtts = [0.0];
@@ -91,40 +119,19 @@ class _ClientsScreenState extends State<ClientsScreen> {
     return new Future(() => result.toDouble());
   }
 
-  List<String> suggestions = [
-    "Apple",
-    "Armidillo",
-    "Actual",
-    "Actuary",
-    "America",
-    "Argentina",
-    "Australia",
-    "Antarctica",
-    "Blueberry",
-    "Cheese",
-    "Danish",
-    "Eclair",
-    "Fudge",
-    "Granola",
-    "Hazelnut",
-    "Ice Cream",
-    "Jely",
-    "Kiwi Fruit",
-    "Lamb",
-    "Macadamia",
-    "Nachos",
-    "Oatmeal",
-    "Palm Oil",
-    "Quail",
-    "Rabbit",
-    "Salad",
-    "T-Bone Steak",
-    "Urid Dal",
-    "Vanilla",
-    "Waffles",
-    "Yam",
-    "Zest"
-  ];
+  List<String> suggestions = [];
+  void getclients() async {
+    suggestions.clear();
+    final messages =
+        await _firestore.collection('nameofclients').getDocuments();
+    for (var msg in messages.documents) {
+      final nn = msg.data['name'].toString();
+      final pp = msg.data['phonenumber'];
+      setState(() {
+        suggestions.add(nn);
+      });
+    }
+  }
 
   SimpleAutoCompleteTextField textField;
   bool showWhichErrorText = false;
@@ -134,6 +141,7 @@ class _ClientsScreenState extends State<ClientsScreen> {
     // TODO: implement initState
     super.initState();
     getcat();
+    getclients();
   }
 
   @override
@@ -141,7 +149,87 @@ class _ClientsScreenState extends State<ClientsScreen> {
     return new Scaffold(
       resizeToAvoidBottomPadding: false,
       appBar: new AppBar(
-        title: new Text('AutoComplete TextField Demo Simple'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: Card(
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 8, right: 8, top: 8, bottom: 8),
+                                child: TextField(
+                                  controller: _textEditingController4,
+                                  keyboardType: TextInputType.emailAddress,
+                                  textAlign: TextAlign.center,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      name = value;
+                                    });
+                                  },
+                                  decoration:
+                                      KTextFieldImputDecoration.copyWith(
+                                          hintText: 'Enter Name Of Client'),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 8, right: 8, top: 8, bottom: 8),
+                                child: TextField(
+                                  controller: _textEditingController5,
+                                  keyboardType: TextInputType.emailAddress,
+                                  textAlign: TextAlign.center,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      phonenumber = value;
+                                    });
+                                  },
+                                  decoration:
+                                      KTextFieldImputDecoration.copyWith(
+                                          hintText:
+                                              'Enter PhoneNumber Of Client'),
+                                ),
+                              ),
+                              MaterialButton(
+                                onPressed: () {
+                                  _firestore.collection('nameofclients').add({
+                                    'name': name,
+                                    'phone': phonenumber,
+                                  });
+                                  _textEditingController4.clear();
+                                  _textEditingController5.clear();
+                                  getclients();
+                                },
+                                child: Text(
+                                  'Send',
+                                  style: TextStyle(
+                                      color: Colors.blue, fontSize: 30),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+              })
+        ],
+        title: new Text(
+          'Clients',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        leading: new IconButton(
+          icon: new Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
       ),
       body: Column(
         children: <Widget>[
@@ -150,27 +238,24 @@ class _ClientsScreenState extends State<ClientsScreen> {
             children: <Widget>[
               Text(mytext),
               FutureBuilder(
-                  builder: (BuildContext context,
-                      AsyncSnapshot<double> qttnumbr) {
+                  builder:
+                      (BuildContext context, AsyncSnapshot<double> qttnumbr) {
                     return Center(
                       child: Row(
-                        mainAxisAlignment:
-                        MainAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
                           Text('Debt:'),
                           Text(
                             '${qttnumbr.data}',
                             style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.blueAccent),
+                                fontSize: 16, color: Colors.blueAccent),
                           ),
                         ],
                       ),
                     );
                   },
                   initialData: 0.0,
-                  future:
-                  getqtt(mytext)),
+                  future: getqtt(mytext)),
             ],
           ),
           Container(
@@ -242,8 +327,7 @@ class _ClientsScreenState extends State<ClientsScreen> {
                   debt = (double.parse(value));
                 });
               },
-              decoration: KTextFieldImputDecoration.copyWith(
-                  hintText: 'Debt'),
+              decoration: KTextFieldImputDecoration.copyWith(hintText: 'Debt'),
             ),
           ),
           Padding(
@@ -257,65 +341,164 @@ class _ClientsScreenState extends State<ClientsScreen> {
                   DebtAnalysis = (double.parse(value));
                 });
               },
-              decoration: KTextFieldImputDecoration.copyWith(
-                  hintText: 'Debt analysis'),
+              decoration:
+                  KTextFieldImputDecoration.copyWith(hintText: 'Debt analysis'),
             ),
           ),
           Center(
-            child: MaterialButton(onPressed: (){
+            child: MaterialButton(
+              onPressed: () {
                 showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Are You Sure?'),
+                        actions: <Widget>[
+                          MaterialButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('cancel'),
+                          ),
+                          MaterialButton(
+                            onPressed: () {
+                              _firestore.collection('clients').add({
+                                'name': mytext,
+                                'product': itemValue,
+                                'qtt': qtt,
+                                'debt': debt,
+                                'den': -DebtAnalysis,
+                              });
+                              setState(() {
+                                qtt = 0.0;
+                                debt = 0.0;
+                                DebtAnalysis = 0.0;
+                              });
+
+                              _textEditingController1.clear();
+                              _textEditingController2.clear();
+                              _textEditingController3.clear();
+
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Yes'),
+                          ),
+                        ],
+                      );
+                    });
+              },
+              child: Text(
+                'Send',
+                style: TextStyle(color: Colors.blue, fontSize: 30),
+              ),
+            ),
+          ),
+          MaterialButton(
+            onPressed: () {
+              gettransactiondate(mytext);
+              print(transaction);
+              showDialog(
                   context: context,
                   builder: (BuildContext context) {
                     return AlertDialog(
-                      title: Text('Are You Sure?'),
-                      actions: <Widget>[
-                        MaterialButton(onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                          child: Text('cancel'),
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0))),
+                      content: Scaffold(
+                        appBar: AppBar(
+                          title: Text(
+                            mytext.toString(),
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          backgroundColor: Colors.white,
                         ),
-                        MaterialButton(onPressed: () {
+                        body: Container(
+                          child: new ListView.builder(
+                              itemCount: transaction.length,
+                              itemBuilder: (BuildContext cntxt, int index) {
+                                return Dismissible(
+                                  background: Material(
+                                    color: Colors.red,
+                                  ),
+                                  onDismissed:
+                                      (DismissDirection direction) async {
+//                                    await _firestore.collection('messages').getDocuments().then((snapshot) {
+//                                      for (DocumentSnapshot ds in snapshot.documents){
+//                                        ds.reference.delete();
+//                                      });
+//                                    }
 
-                          _firestore.collection('clients').add({
-                            'name':mytext,
-                            'product':itemValue,
-                            'qtt':qtt,
-                            'debt':debt,
-                            'den':-DebtAnalysis,
-
-                          });
-                          setState(() {
-                            qtt=0.0;
-                            debt=0.0;
-                            DebtAnalysis=0.0;
-                          });
-
-
-                          _textEditingController1.clear();
-                          _textEditingController2.clear();
-                          _textEditingController3.clear();
-
-
-
-
-                          Navigator.of(context).pop();
-                        },
-                          child: Text('Yes'),
+                                    print('${transaction[index]['id']}');
+                                    print(transaction[index]);
+                                    await _firestore
+                                        .collection('clients')
+                                        .document('${transaction[index]['id']}')
+                                        .delete();
+                                    gettransactiondate(mytext);
+                                    transaction.remove(transaction[index]);
+                                  },
+                                  key: Key(transaction[index].toString()),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Container(
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            '${transaction[index]['debt'].toString()}',
+                                            style:
+                                                TextStyle(color: Colors.green),
+                                          ),
+                                        ),
+                                        decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color: Colors.black)),
+                                        width: 85,
+                                      ),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color: Colors.black)),
+                                        width: 85,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            '${transaction[index]['den'].toString()}',
+                                            style: TextStyle(color: Colors.red),
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color: Colors.black)),
+                                        width: 50,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            '${transaction[index]['product'].toString()}',
+                                            style: TextStyle(
+                                                color: Colors.blueAccent),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }),
                         ),
-
-                      ],
+                      ),
                     );
-
-                  }
-                );
-
+                  });
             },
-            child: Text('Send',style: TextStyle(color: Colors.blue,fontSize: 30),),
-
+            child: Text(
+              'Transaction Report',
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-
-
         ],
       ),
     );
